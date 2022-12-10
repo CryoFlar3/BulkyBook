9336d353939d44ff3a7b42bf63291c7abb53d7be
Nyjel Dukes <ndukes@computermentors.org>
2022-12-10 18:57:31 -0500

    Created Category Edit View

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -35,5 +35,32 @@ namespace BulkyBookWeb.Controllers
             }
             return View(obj);
         }
+
+        // GET 
+        public IActionResult Edit(int id) {
+            if(id==null || id == 0) {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null) {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj) {
+            if (obj.Name == obj.DisplayOrder.ToString()) {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot be the same as the Name");
+            }
+            if (ModelState.IsValid) {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
