7c935e6ac9417886f8beb4d8d8d9cd99425f3cdf
Nyjel Dukes <ndukes@computermentors.org>
2022-12-11 20:14:34 -0500

    Added a Delete action and view to Category

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -25,8 +25,8 @@ namespace BulkyBookWeb.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj) {
-            if(obj.Name == obj.DisplayOrder.ToString()) {
-                ModelState.AddModelError("CustomError","The DisplayOrder cannot be the same as the Name");
+            if (obj.Name == obj.DisplayOrder.ToString()) {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot be the same as the Name");
             }
             if (ModelState.IsValid) {
                 _db.Categories.Add(obj);
@@ -38,7 +38,7 @@ namespace BulkyBookWeb.Controllers
 
         // GET 
         public IActionResult Edit(int id) {
-            if(id==null || id == 0) {
+            if (id == null || id == 0) {
                 return NotFound();
             }
             var categoryFromDb = _db.Categories.Find(id);
@@ -56,11 +56,36 @@ namespace BulkyBookWeb.Controllers
                 ModelState.AddModelError("CustomError", "The DisplayOrder cannot be the same as the Name");
             }
             if (ModelState.IsValid) {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        // GET 
+        public IActionResult Delete(int? id) {
+            if (id == null || id == 0) {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null) {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        // POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id) {
+            var obj = _db.Categories.Find(id);
+            if (obj == null) {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
