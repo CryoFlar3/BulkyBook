51a515f7e238ad19c284767e82d690eb01c1ec79
Nyjel Dukes <ndukes@computermentors.org>
2022-12-21 17:06:39 -0500

    Restructured to N-Tier Architecture

---
+++ BulkyBook.Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Utility
+{
+    // SD = Static Details. This will be for program constants.
+    public static class SD
+    {
+    }
+}
