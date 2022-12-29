3ef3e4a7283acbc786f3c22bcf967c99c2304855
Nyjel Dukes <ndukes@computermentors.org>
2022-12-29 15:50:32 -0500

    Created Upsert page and action for Product

---
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers;
+[Area("Admin")]
+
+public class ProductController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ProductController(IUnitOfWork unitOfWork) {
+        _unitOfWork = unitOfWork;
+    }
+
+    public IActionResult Index() {
+        IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
+        return View(objCoverTypeList);
+    }
+
+    // GET 
+    public IActionResult Upsert(int id) {
+        Product product = new();
+        if (id == null || id == 0) {
+            // Create product
+            return View(product);
+        } else {
+            // Update product
+
+        }
+
+        return View(product);
+    }
+    // POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Upsert(CoverType obj) {
+        if (ModelState.IsValid) {
+            _unitOfWork.CoverType.Update(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "CoverType updated successfully";
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
+    // GET 
+    public IActionResult Delete(int? id) {
+        if (id == null || id == 0) {
+            return NotFound();
+        }
+        var categoryFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+
+        if (categoryFromDbFirst == null) {
+            return NotFound();
+        }
+        return View(categoryFromDbFirst);
+    }
+    // POST
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePost(int? id) {
+        var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+        if (obj == null) {
+            return NotFound();
+        }
+        _unitOfWork.CoverType.Remove(obj);
+        _unitOfWork.Save();
+        TempData["success"] = "CoverType deleted successfully";
+        return RedirectToAction("Index");
+    }
+}
+
