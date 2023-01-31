76a87a0dad9f12dae40ac7fb4b5c7a45135891aa
Nyjel Dukes <ndukes@computermentors.org>
2023-01-30 20:11:47 -0500

    Cart total calculates correctly

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -6,37 +6,38 @@ using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
-    [Area("Customer")]
-    [Authorize]
-    public class CartController : Controller
-    {
-        private readonly IUnitOfWork _unitOfWork;
-        public ShoppingCartVM ShoppingCartVM { get; set; }
-        public int OrderTotal { get; set; }
-        public CartController(IUnitOfWork unitOfWork) {
-            _unitOfWork = unitOfWork;
-        }
-        public IActionResult Index() {
+	[Area("Customer")]
+	[Authorize]
+	public class CartController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public ShoppingCartVM ShoppingCartVM { get; set; }
+		public int OrderTotal { get; set; }
+		public CartController(IUnitOfWork unitOfWork) {
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult Index() {
 
-            var claimsIdentity = (ClaimsIdentity)User.Identity;
-            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
-            ShoppingCartVM = new ShoppingCartVM() {
-                ListCart = _unitOfWork.ShoppingCart.GetAll(u=>u.ApplicationUserId==claim.Value, includeProperties: "Product")
-            };
-            foreach(var cart in ShoppingCartVM.ListCart) {
-                cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
-            }
-            return View(ShoppingCartVM);
-        }
-        private double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100) { 
-            if (quantity <= 50) {
-                return price;
-            } else if (quantity <= 100) {
-                return price50;
-            } else {
-                return price100;
-            }
-        }
-    }
+			ShoppingCartVM = new ShoppingCartVM() {
+				ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "Product")
+			};
+			foreach (var cart in ShoppingCartVM.ListCart) {
+				cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+				ShoppingCartVM.CartTotal += (cart.Price * cart.Count);
+			}
+			return View(ShoppingCartVM);
+		}
+		private double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100) {
+			if (quantity <= 50) {
+				return price;
+			} else if (quantity <= 100) {
+				return price50;
+			} else {
+				return price100;
+			}
+		}
+	}
 }
