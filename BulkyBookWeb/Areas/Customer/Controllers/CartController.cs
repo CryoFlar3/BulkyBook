ba6b20ac51e24dd4f1c93ac119d73102dfd1d40f
Nyjel Dukes <ndukes@computermentors.org>
2023-01-30 20:07:28 -0500

    Price now scales with quantity

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -12,6 +12,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         public ShoppingCartVM ShoppingCartVM { get; set; }
+        public int OrderTotal { get; set; }
         public CartController(IUnitOfWork unitOfWork) {
             _unitOfWork = unitOfWork;
         }
@@ -23,8 +24,19 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             ShoppingCartVM = new ShoppingCartVM() {
                 ListCart = _unitOfWork.ShoppingCart.GetAll(u=>u.ApplicationUserId==claim.Value, includeProperties: "Product")
             };
-
+            foreach(var cart in ShoppingCartVM.ListCart) {
+                cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+            }
             return View(ShoppingCartVM);
+        }
+        private double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100) { 
+            if (quantity <= 50) {
+                return price;
+            } else if (quantity <= 100) {
+                return price50;
+            } else {
+                return price100;
+            }
         }
     }
 }
