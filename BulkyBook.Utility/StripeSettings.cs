a7ff5980b66dcca0ba85ee1027d118fe382a52a0
Nyjel Dukes <ndukes@computermentors.org>
2023-02-08 15:31:09 -0500

    Added Stripe Api to project

---
+++ BulkyBook.Utility/StripeSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Utility
+{
+    public class StripeSettings
+    {
+        public string SectretKey { get; set; }
+        public string PublishableKey { get; set; }
+
+    }
+}
