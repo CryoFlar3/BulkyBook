4775e15eaf9ce0819685eadd112ec31a1a86192c
Nyjel Dukes <ndukes@computermentors.org>
2023-02-07 22:39:38 -0500

    Fixed some formatting

--- BulkyBook.DataAccess/Repository/OrderHeaderRepository.cs
+++ BulkyBook.DataAccess/Repository/OrderHeaderRepository.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace BulkyBook.DataAccess.Repository
 {
     public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
-	{
+    {
         private ApplicationDbContext _db;
 
         public OrderHeaderRepository(ApplicationDbContext db) : base(db) {
@@ -20,14 +20,14 @@ namespace BulkyBook.DataAccess.Repository
             _db.OrderHeaders.Update(obj);
         }
 
-		public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null) {
-			var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null) {
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
             if (orderFromDb != null) {
                 orderFromDb.OrderStatus = orderStatus;
-                if(paymentStatus != null) {
+                if (paymentStatus != null) {
                     orderFromDb.PaymentStatus = paymentStatus;
                 }
             }
-		}
-	}
+        }
+    }
 }
