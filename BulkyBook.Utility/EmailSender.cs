6445b47fe120030ba1bea1fd10620d493b798aea
Nyjel Dukes <ndukes@computermentors.org>
2023-03-20 22:26:21 -0400

    Added email client data

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+using Org.BouncyCastle.Tls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +13,20 @@ namespace BulkyBook.Utility
     public class EmailSender : IEmailSender
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage) {
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("hello@computermentors.org"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body=new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+            // Send email
+            using(var emailClient = new SmtpClient()) {
+                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                emailClient.Authenticate("nyjel.dukes@gmail.com", "Nd0198$");
+                emailClient.Send(emailToSend);
+                emailClient.Disconnect(true);
+            }
+
             return Task.CompletedTask;
         }
     }
